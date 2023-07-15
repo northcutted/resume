cda82058b05280576c13f9f2def1956b57de7946
Eddie Northcutt <35792077+northcutted@users.noreply.github.com>
2023-07-15 21:56:25 +0000

    fix: add styling

--- convert.js
+++ convert.js
@@ -3,4 +3,17 @@ const fs = require('fs');
 const converter = new showdown.Converter();
 const text = fs.readFileSync('resume.md', 'utf8');
 const html = converter.makeHtml(text);
-fs.writeFileSync('index.html', html);
+
+const htmlWithStyles = `
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    ${html}
+</body>
+</html>
+`;
+
+fs.writeFileSync('index.html', htmlWithStyles);
